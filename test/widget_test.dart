fa797cc9883466dc7dd631981c7ad7a1be83abf4
NaveenKesavaraj <naveen.kesavaraj@syncfusion.com>
2025-03-25 11:01:55 +0530

    FLUT-948126-[others]: Updated the read me file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:datagrid_simple_sample/main.dart';
+import 'package:how_to_draw_custom_thumb_in_range_slider/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
